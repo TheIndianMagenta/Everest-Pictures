55f326c2afba9e732ba940804f91c8b4fd4b5840
SamarthAnnigeri <Samarthgrand@gmail.com>
2022-10-31 11:47:17 -0700

    Initial Commit

---
+++ Everest Pictures/Everest_PicturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Everest_PicturesApp.swift
+//  Everest Pictures
+//
+//  Created by Samarth Annigeri on 10/31/22.
+//
+
+import SwiftUI
+
+@main
+struct Everest_PicturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
